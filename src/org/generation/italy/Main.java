141ba1e11c4ed47e7ea9ea77409f8e44a3d4ce2f
enzoc <enzoc@encol-pc>
2023-12-04 10:05:07 +0100

    classe Prodotto

---
+++ src/org/generation/italy/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Scrivere un programma che simula il funzionamento di un negozio online.
+
+Le funzionalità da realizzare sono:
+
+Inserimento prodotto: consente di inserire i dati di un nuovo prodotto al negozio. 
+Ogni prodotto è caratterizzato da:
+codice (String): codice identificativo del prodotto (non è possibile inserire due prodotti con lo stesso codice)
+Descrizione (String)
+Prezzo (float)
+Sconto (float): percentuale di sconto da applicare eventualmente al prodotto (di default 0)
+Quantità disponibile (int)
+
+Elenco prodotti: per ogni prodotto vengono visualizzate le informazioni
+Applica sconto: si inserisce il codice di un prodotto e la percentuale di sconto da applicare
+Vendita prodotti:
+si chiede all'utente di scegliere un prodotto dall'elenco (inserendo il codice);
+si visualizzano le informazioni del prodotto scelto;
+si chiede la quantità da acquistare;
+se la quantità disponibile è sufficiente, il prodotto si aggiunge al "carrello della spesa" applicando eventuali sconti (in questo caso mostrare sia il prezzo originale che il prezzo scontato)
+si visualizza il carrello attuale
+si chiede se si vuole procedere all'acquisto (in caso positivo si mostra il totale e si aggiornano le quantità disponibili)
+ */
+
+package org.generation.italy;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/generation/italy/Prodotto.java
@@ -0,0 +1,69 @@
+package org.generation.italy;
+
+public class Prodotto {
+	private String codice, descrizione;
+	private float prezzo, sconto;
+	private int quantitàDisponibile;
+	
+	//costruttore
+	public Prodotto(String codice, String descrizione, float prezzo, int quantitàDisponibile) {		
+		//valori parametrici
+		this.codice = codice;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.quantitàDisponibile = quantitàDisponibile;
+		//valore predefinito
+		this.sconto=0;		
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		if (!codice.isEmpty())
+			this.codice = codice;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		if (!descrizione.isEmpty())
+			this.descrizione = descrizione;
+	}
+
+	public float getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(float prezzo) {
+		if (prezzo>0)
+			this.prezzo = prezzo;
+	}
+
+	public float getSconto() {
+		return sconto;
+	}
+
+	public void setSconto(float sconto) {
+		if (sconto>=0 && sconto<=100)
+			this.sconto = sconto;
+	}
+
+	public int getQuantitàDisponibile() {
+		return quantitàDisponibile;
+	}
+
+	public void setQuantitàDisponibile(int quantitàDisponibile) {
+		if (quantitàDisponibile>0)
+			this.quantitàDisponibile = quantitàDisponibile;
+	}
+	
+	
+	
+	
+	
+	
+}
